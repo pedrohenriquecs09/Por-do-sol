b1b3f93aa6b80a052cbbf59284176e25d81cf2d9
pedrohenriquecs09 <pedro.silva1310@escola.pr.gov.br>
2024-11-13 08:53:13 -0300

    13/11

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let posicaoVertial;
+
+function setup() {
+  createCanvas (500, 500);
+    posicaoVertical = 250;
+    }
+
+    function draw() {
+      if (posicaoVertical < 400) {
+          background("lightblue");
+              posicaoVertical = posicaoVertical + 1;
+                }
+                  fill("orange");
+                    background;
+                      circle(250, posicaoVertical, 300);
+                        fill("#2007FF");
+                          rect(0, 250, 500, 350);
+                            fill("#FFC107");
+                              rect(0, 400, let posicaoVertial;
+
+                              function setup() {
+                                createCanvas (500, 500);
+                                  posicaoVertical = 250;
+                                  }
+
+                                  function draw() {
